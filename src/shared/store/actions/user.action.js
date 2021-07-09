87bff2ef6e344470a65017dfde7c2195aa3ef2fa
Irem Kenar <ikenar@kazandirio-dev.com>
2021-07-09 16:38:44 +0300

    Added user actions

---
+++ src/shared/store/actions/user.action.js
@@ -0,0 +1,21 @@
+import { userConstant } from "../constants/user.constant";
+
+const loadUserAction = (payload) => ({
+  type: userConstant.LOAD_USER,
+  payload,
+});
+
+const loadUserSuccessAction = (payload) => ({
+  type: userConstant.LOAD_USER_SUCCESS,
+  payload,
+});
+
+const loadUserFailAction = () => ({
+  type: userConstant.LOAD_USER_FAIL,
+});
+
+export const userActions = {
+  loadUserAction,
+  loadUserSuccessAction,
+  loadUserFailAction,
+};
